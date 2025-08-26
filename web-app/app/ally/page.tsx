a0f6ea1c7862fc380967fb35365a8cc87051ade6
aryanyan <31175254+AryanRai@users.noreply.github.com>
2025-08-26 19:43:53 +1000

    CleanUp

--- web-app/app/ally/page.tsx
+++
@@ -1,5 +0,0 @@
-import AllyRemoteDashboard from '@/components/ally-remote-dashboard';
-
-export default function AllyPage() {
-  return <AllyRemoteDashboard />;
-}
--- web-app/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import AllyRemoteDashboard from '@/components/ally-remote-dashboard';
-
-export default function Home() {
-  return <AllyRemoteDashboard />;
-}
